494c3debee141b40b36e7382b4731abfe8f7971f
Roman Zhylko <roman.zhilko@gmail.com>
2020-10-23 20:52:39 +0300

    Initial commit

---
+++ Lab1/app/src/main/java/by/bsu/lab1/MainActivity.java
@@ -0,0 +1,41 @@
+package by.bsu.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private EditText input;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button submit = findViewById(R.id.submitButton);
+        textView = findViewById(R.id.textView);
+        if (getIntent().getStringExtra("number") != null)
+            textView.setText(getIntent().getStringExtra("number"));
+        input = findViewById(R.id.input);
+        submit.setOnClickListener(onSubmitClickListener);
+    }
+
+    View.OnClickListener onSubmitClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+            if (input.getText().toString().isEmpty())
+                Toast.makeText(getApplicationContext(), "Input number first!", Toast.LENGTH_SHORT).show();
+            else {
+                intent.putExtra("number", Integer.parseInt(input.getText().toString()) + Integer.parseInt(textView.getText().toString()) + "");
+                startActivity(intent);
+            }
+        }
+    };
+}
